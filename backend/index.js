8681943231f62b763480bd893d14a72abf39c979
divyanshu <divyanshulohar2002@gmail.com>
2023-09-06 23:46:10 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,56 @@
-const express = require("express")
+const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
+app.use(express.json()); // Parse JSON requests
 
+// Define a mongoose schema for your data
+const dataSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
 
-app.get("/getData", (req,res)=>{
-    res.send("Hello world");
-})
+// Create a mongoose model based on the schema
+const Data = mongoose.model("Data", dataSchema);
 
+// Connect to the MongoDB database
+mongoose
+  .connect("mongodb+srv://divyanshu:1234@ecom.6l8v2zu.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database");
+    // Add some sample data
+    for (let i = 1; i <= 10; i++) {
+      const newData = new Data({
+        name: `Person ${i}`,
+        age: 20 + i,
+      });
+      newData.save().then(() => {
+        console.log(`Added: ${newData}`);
+      });
+    }
+  })
+  .catch((error) => {
+    console.error("Connection to the database failed:", error);
+  });
 
-mongoose.connect("")
+// Define a route to fetch the data
+app.get("/getData", async (req, res) => {
+  try {
+    const data = await Data.find({});
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Error fetching data" });
+  }
+});
 
-
-app.listen(3000, ()=>{
-    console.log("server started at port 3000");
-})
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const menubtn = document.getElementById("menubtn")
-const menusm  = document.getElementById("menusm")
+const menubtn = document.getElementById("menubtn");
+const menusm = document.getElementById("menusm");
 
 menubtn.onclick = function () {
-    if (menusm.style.display === "none" || menusm.style.display === "") {
-      menusm.style.display = "flex";
-    } else {
-      menusm.style.display = "none";
-    }
-  };
+  if (menusm.style.display === "none" || menusm.style.display === "") {
+    menusm.style.display = "flex";
+    fetchData();
+  } else {
+    menusm.style.display = "none";
+  }
+};
+
+function fetchData() {
+  fetch("http://localhost:3000/getData")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json(); // Parse the response as JSON
+    })
+    .then((data) => {
+      // Handle the fetched data here
+      console.log(data.message); // Access the message property from the JSON
+    })
+    .catch((error) => {
+      // Handle errors here
+      console.error("Fetch error:", error);
+    });
+}
