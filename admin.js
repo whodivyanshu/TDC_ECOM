9b9ac566ab3ec96e3c9aabb987fbad866d2dc774
divyanshu <divyanshulohar2002@gmail.com>
2023-09-10 17:20:32 +0530

    commit by divyanshu

--- admin.js
+++ admin.js
@@ -1,35 +1,46 @@
 function add() {
-    const name = document.getElementById("name").value;
-    const imageurl = document.getElementById("imgurl").value;
-    const price = document.getElementById("price").value;
-    const description = document.getElementById("desc").value;
-  
-    const newData = {
-      name,
-      imageurl,
-      price,
-      description,
-    };
-  
-    fetch("http://localhost:3000/addData", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newData),
+  const name = document.getElementById("name").value;
+  const imageurl = document.getElementById("imgurl").value;
+  const price = document.getElementById("price").value;
+  const description = document.getElementById("desc").value;
+
+  const newData = {
+    name,
+    imageurl,
+    price,
+    description,
+  };
+
+  fetch("http://localhost:3000/addData", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newData),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        console.log("Data added:", data);
-      })
-      .catch((error) => {
-        console.error("Error adding data:", error);
-      });
-      window.location.reaload();
+    .then((data) => {
+      console.log("Data added:", data);
+    })
+    .catch((error) => {
+      console.error("Error adding data:", error);
+    });
+  window.location.reaload();
+}
+
+const addWS = async () => {
+  const response = await fetch("http://localhost:3000/run-puppeteer", {
+    method: "GET",
+  });
+  if (response.ok) {
+    console.log("Data added successfully.");
+  } else {
+    console.error("Failed to add data.");
   }
-  
+  window.location.reload();
+};
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import puppeteer from "puppeteer";
+import cheerio from "cheerio";
+import fetch from "node-fetch";
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
@@ -18,6 +20,7 @@ const dataSchema = new mongoose.Schema({
 const adminSchema = new mongoose.Schema({
   number: String,
   id: String,
+  searchName: String,
 });
 
 const Data = mongoose.model("Data", dataSchema);
@@ -76,6 +79,67 @@ app.get("/getNumber", async (req, res) => {
   } catch (error) {
     console.error("Error fetching data:", error);
     res.status(500).json({ error: "Error fetching data" });
+  }
+});
+
+app.get("/run-puppeteer", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    const url = "https://www.myntra.com/";
+    await page.goto(url);
+
+    await page.type(".desktop-searchBar", "Mens Footwear");
+    await page.keyboard.press("Enter");
+
+    await page.waitForTimeout(5000);
+    const content = await page.content();
+    const $ = cheerio.load(content);
+    let products = [];
+
+    $(".product-base").each(async (index, element) => {
+      if (index < 10) {
+        const innerText = $(element).find(".product-product").text();
+        const src = $(element)
+          .find(".product-imageSliderContainer img")
+          .attr("src");
+
+        const price = $(element).find(".product-discountedPrice").text();
+        const money = parseInt(price, 10);
+
+        const newData = {
+          name: innerText,
+          imageurl: src,
+          price: price,
+          description: "",
+        };
+
+        try {
+          const response = await fetch("http://localhost:3000/addData", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newData),
+          });
+
+          if (response.ok) {
+            console.log("Data added successfully.");
+          } else {
+            console.error("Failed to add data.");
+          }
+        } catch (error) {
+          console.error("Error while sending data:", error);
+        }
+      }
+    });
+
+    await browser.close();
+    res.send("Puppeteer script completed successfully");
+  } catch (error) {
+    console.error("Error running Puppeteer script:", error);
+    res.status(500).json({ error: "Error running Puppeteer script" });
   }
 });
 
